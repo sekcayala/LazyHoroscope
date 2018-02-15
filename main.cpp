d168a7e99da5c3a258d3bbd24f473c2e105a800d
sekcayala <aayala39@mail.csuchico.edu>
2018-02-14 18:31:26 -0800

    Trial 1

--- main.cpp
+++ main.cpp
@@ -19,6 +19,27 @@ int lucky;
    cout<<"Welcome," <<first [0]<<"." <<last [0]<< "." ", here is your fortune..." <<endl;
 
    cout<<"Your lucky number is " <<first.length()+last.length()<<endl;
+
+   char a, e, i, o, u;
+
+   if (first[0] = (a || e || i || o || u))
+   {
+   cout<<"You are destined to be famous!\n";
+   }
+   else
+   {
+   cout<< "You should keep a low profile.\n";
+   }
+   string word;
+   cout<<word[0]<<endl;
+   cin>>word;
+
+   int len=last.length();
+   if (last[len-1]= (a || e || i || o || u));
+   {
+   cout<<"You have already met your true love.\n";
+   }
+   cout<<"Have a good day!"<<endl;
   //get user input
 
   //tell fortune
