532d67cd906fd099c56a8bdb71e335137438141f
sekcayala <aayala39@mail.csuchico.edu>
2018-02-14 20:00:44 -0800

    please

--- main.cpp
+++ main.cpp
@@ -10,33 +10,33 @@ int lucky;
 
   //declare variables
    string first, last;
-   cout<<"What is your first name? \n";
+   cout<<"What is your first name?\n";
    cin>>first;
 
-   cout<<"What is your last name? \n";
+   cout<<"What is your last name?\n";
    cin>>last;
 
-   cout<<"Welcome," <<first [0]<<"." <<last [0]<< "." ", here is your fortune..." <<endl;
+   cout<<"Welcome, " <<first [0]<<"." <<last [0]<< "." ", here is your fortune..." <<endl;
 
-   cout<<"Your lucky number is " <<first.length()+last.length()<<endl;
+   cout<<"your lucky number is " <<first.length()+last.length()<<endl;
 
    char a, e, i, o, u;
 
    if (first[0] = (a || e || i || o || u))
    {
-   cout<<"You are destined to be famous!\n";
+   cout<<"you are destined to be famous!\n";
    }
    else
    {
-   cout<< "You should keep a low profile.\n";
+   cout<< "you should keep a low profile.\n";
    }
 
    int len=last.length();
    if (last[len-1]= (a || e || i || o || u));
    {
-   cout<<"You have already met your true love.\n";
+   cout<<"you have already met your true love.\n";
    }
-   cout<<"Have a good day!"<<endl;
+   cout<<"have a good day!"<<endl;
   //get user input
 
   //tell fortune
